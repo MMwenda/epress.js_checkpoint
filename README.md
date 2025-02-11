f927a88c5e307417a234b92a14b9d5f158087174
MMwenda <maur1cemmwenda@gmail.com>
2025-02-11 01:34:58 +0100

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-### UNFINISHED PROJECT
+### Express.js Project
+This is a simple and functional web application built using Express.js. The application includes four main pages:
+- **Home**
+- **Our Services**
+- **Contact Us**
+- **Error**
+
+This web app uses a `Custom Access Restriction Middleware`: The application uses custom middleware to restrict access to the site during working hours (Monday to Friday, 9:00 AM to 5:00 PM). 
+This feature ensures the site is only accessible during specific business hours.
+
+
+The project also uses `ESLint` to maintain code quality and consistency. ESLint helps in `identifying` and `fixing problems` in the `JavaScript code`, making the development process smoother and more efficient.
+
--- myapp/app.js
+++ myapp/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
@@ -8,8 +9,8 @@ function checkWorkingHours(req, res, next) {
   const day = now.getDay(); // 0 (Sunday) to 6 (Saturday)
   const hour = now.getHours(); // 0 to 23
 
-  const isWorkingDay = day >= 1 && day <= 5;
-  const isWorkingHour = hour >= 9 && hour <= 17;
+  const isWorkingDay = day >= 1 && day <= 5; // Monday to Friday
+  const isWorkingHour = hour >= 9 && hour <= 17; // 9am to 5pm
 
   if (isWorkingDay && isWorkingHour) {
     next();
@@ -19,16 +20,16 @@ function checkWorkingHours(req, res, next) {
   }
 }
 
-// Serve static files
-app.use(express.static(__dirname + '/public'));
+// Serve everything on the public folder as static files {css, js, images}
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Apply middleware to all routes
-app.use(checkWorkingHours);
+app.use(checkWorkingHours); //this will be applied to all routes because it is before the routes
 
 // Routes to HTML files
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
-app.get('/services', (req, res) => res.sendFile(__dirname + '/public/services.html'));
-app.get('/contact', (req, res) => res.sendFile(__dirname + '/public/contact.html'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/services', (req, res) => res.sendFile(path.join(__dirname, '/public/services.html')));
+app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, '/public/contact.html')));
 
 
 app.listen(PORT, () => {
